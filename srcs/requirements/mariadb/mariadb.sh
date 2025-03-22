1a9aca34c00bd271470a7706e68ce42e308f8cd2
zech-chi <machinelearning1.618@gmail.com>
2025-03-22 01:46:04 +0000

    almost done

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -33,9 +33,6 @@ mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
 mysql -uroot -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%';"
 
-# for mariadb_backup
-mysql -uroot -e "GRANT RELOAD, PROCESS ON *.* TO '$SQL_USER'@'%';"
-
 # Allow root access from any host
 mysql -uroot -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
