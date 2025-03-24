a0672478575c722d692a3f529a9ae5091248ca48
zech-chi <machinelearning1.618@gmail.com>
2025-03-24 19:01:27 -0400

    done 100.0% mariadb

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -2,10 +2,13 @@
 
 echo "Start MariaDB service"
 
+# Set permissions for MariaDB data directory
 chown -R mysql:mysql /var/lib/mysql
 chmod 755 /var/lib/mysql
 
+# /run/mysqld used for the MySQL socket
 mkdir -p /run/mysqld
+# Change the ownership to user mysql
 chown -R mysql:mysql /run/mysqld
 
 service mariadb start
@@ -14,19 +17,28 @@ echo "Waiting for MariaDB to start..."
 sleep 10
 echo "MariaDB is up!"
 
+# get passwords
 DB_PASSWORD="$(cat /run/secrets/db_password)"
 DB_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)"
 
+# create database
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
+# create user and grants it all privileges 
 mysql -uroot -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%';"
 
+# create root user
 mysql -uroot -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+# grants it full privileges.
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+# for local connections.
 mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
+# Applies the changes to the privileges
 mysql -uroot -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+# Gracefully shuts down the MariaDB server
 mysqladmin -uroot -p"$DB_ROOT_PASSWORD" shutdown
 
 echo "Starting MariaDB in the foreground..."
+# keeps running mysqld_safe as the main process in the container.
 exec mysqld_safe
