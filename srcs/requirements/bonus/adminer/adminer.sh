311ac179086f35e220ce9928b60439ba25d88aa4
zech-chi <machinelearning1.618@gmail.com>
2025-03-24 20:09:11 -0400

    done 100.0% adminer

--- srcs/requirements/bonus/adminer/adminer.sh
+++ srcs/requirements/bonus/adminer/adminer.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+# to store the Adminer PHP file.
 mkdir -p /var/www/html/adminer
 
 cd /var/www/html/adminer
 
+# -L for redirction purpose
+# download the Adminer PHP script and saves it as index.php in the current directory (/var/www/html/adminer).
 curl -L -o index.php https://github.com/vrana/adminer/releases/download/v4.7.8/adminer-4.7.8.php
 
 echo "Starting Adminer in the foreground..."
 
+# -S option starts PHP server
+# -t option specifies the document root
 php -S 0.0.0.0:8080 -t /var/www/html/adminer
