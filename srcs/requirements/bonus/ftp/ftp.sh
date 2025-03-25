5154cc7272d661accc3b9dfc74a5c41dc01374f3
zech-chi <machinelearning1.618@gmail.com>
2025-03-24 20:04:16 -0400

    done 100.0% ftp

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+# create user
 useradd -m "$FTP_USER"
 
+# get password
 FTP_PASS="$(cat /run/secrets/ftp_password)"
+# add password
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 
-# Create the required directory for secure_chroot_dir
+# Create the required directory for secure_chroot_dir 
+# for jailed (chrooted) FTP users (for security reasons)
 mkdir -p /var/run/vsftpd/empty
 chmod 755 /var/run/vsftpd/empty
 
 echo "Starting FTP in the foreground..."
+# vsftpd: secure FTP server that allows clients to connect and transfer files
 vsftpd /etc/vsftpd.conf
